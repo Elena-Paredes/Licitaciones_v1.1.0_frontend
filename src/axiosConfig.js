283b1ed6aead9689576adf8ad708b382f716d338
Elena Paredes <chawy_07@hotmail.com>
2024-09-03 17:41:03 -0600

    Se modificó baseURL para conexión de Backend y Frontend en producción

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,8 @@
+/* /frontend/src/axiosConfig.js */
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3001', 
+  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:3001', 
   withCredentials: true
 });
 
