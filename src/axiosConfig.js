73f5c32a98c386f2c84976d617041cf77e52779a
Elena Paredes <chawy_07@hotmail.com>
2024-09-04 11:28:20 -0600

    Se acompleto el código axiosConfig en el que faltaba exporta la variable api

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -4,3 +4,4 @@ import axios from 'axios'; // Si estás usando la librería directamente
 const api = axios.create({
   baseURL: "http://licitacionesv110background-production-be53.up.railway.app",
 });
+export default api;
