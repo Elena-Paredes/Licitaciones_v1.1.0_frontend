b556e81019ca45dc07ce511f995df5db36785ff0
Elena Paredes <chawy_07@hotmail.com>
2024-09-04 11:32:40 -0600

    Se cambio la variable axios por la palabra api en SingUp y SignIn IV

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,6 +2,6 @@
 import axios from 'axios'; // Si estás usando la librería directamente
 
 const api = axios.create({
-  baseURL: "http://licitacionesv110background-production-be53.up.railway.app",
+  baseURL: "https://licitacionesv110background-production-be53.up.railway.app",
 });
 export default api;
